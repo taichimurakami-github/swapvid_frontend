700360696c4af34d93f163aa93ff6de1f52145cd
taichi.murakami <tchi0017@gmail.com>
2023-08-14 22:33:55 +0900

    Add handlers for touch inputs

--- src/hooks/useDraggable.ts
+++ src/hooks/useDraggable.ts
@@ -1,7 +1,7 @@
 import React, { MouseEvent, useCallback, useRef } from "react";
 
 export function useHandleClickWithDrag<T extends HTMLElement>(
-  mouseUpHandler?: (e?: MouseEvent<T>) => void,
+  mouseUpHandler?: (e?: Event) => void,
   interval_ms?: number
 ) {
   const INTERVAL_MS = interval_ms ?? 100;
@@ -12,7 +12,20 @@ export function useHandleClickWithDrag<T extends HTMLElement>(
   }, [mouseDownAt]);
 
   const handleMouseUp = useCallback(
-    (e: MouseEvent<T>) => {
+    (e: Event) => {
+      const now = Date.now();
+
+      // initialのmouseDownAtは0なので，Math.absを使用すると強制的に10^8オーダーの自然数になることで処理を回避
+      if (Math.abs(now - mouseDownAt.current) < INTERVAL_MS && mouseUpHandler) {
+        mouseUpHandler(e);
+        mouseDownAt.current = 0;
+      }
+    },
+    [mouseDownAt]
+  );
+
+  const handleTouchEnd = useCallback(
+    (e: TouchEvent) => {
       const now = Date.now();
 
       // initialのmouseDownAtは0なので，Math.absを使用すると強制的に10^8オーダーの自然数になることで処理を回避
@@ -27,5 +40,7 @@ export function useHandleClickWithDrag<T extends HTMLElement>(
   return {
     onMouseDown: handleMouseDown,
     onMouseUp: handleMouseUp,
+    onTouchStart: handleMouseDown,
+    onTouchEnd: handleTouchEnd,
   };
 }
