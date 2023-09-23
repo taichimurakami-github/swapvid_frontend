3f004a3d391ab9b904dcac361ad4596d5ea873b2
taichi.murakami <tchi0017@gmail.com>
2023-09-23 20:45:00 +0900

    [Experimental] Add intersection observer hook

---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,62 @@
+import React, { RefObject, useEffect, useRef, useState } from "react";
+
+export default function useIntersectionObserver(
+  observerTarget: RefObject<HTMLElement>,
+  observerRoot: RefObject<HTMLElement>,
+  validIntersectRatioInterval = 0.1,
+  observerOptions?: {
+    rootMargin?: string;
+    threshold?: number | number[];
+  }
+) {
+  const [intersectionEntry, setIntersectionEntry] =
+    useState<IntersectionObserverEntry>();
+  const observer = useRef<null | IntersectionObserver>(null);
+  const prevObserverOptions = useRef<IntersectionObserverInit>();
+
+  useEffect(() => {
+    if (observerTarget.current && observerRoot.current) {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+
+      observer.current = new IntersectionObserver(
+        ([entry]) => {
+          const intersectRatioDiffAbs =
+            intersectionEntry === undefined
+              ? 0
+              : Math.abs(
+                  intersectionEntry.intersectionRatio - entry.intersectionRatio
+                );
+
+          if (
+            !intersectionEntry ||
+            intersectRatioDiffAbs >= validIntersectRatioInterval
+          ) {
+            setIntersectionEntry(entry);
+          }
+        },
+        {
+          root: observerRoot.current,
+          threshold: (() => {
+            const thresholds = [];
+            const numSteps = 20;
+
+            for (let i = 1.0; i <= numSteps; i++) {
+              const ratio = i / numSteps;
+              thresholds.push(ratio);
+            }
+
+            thresholds.push(0);
+            return thresholds;
+          })(),
+        }
+      );
+
+      observer.current.observe(observerTarget.current);
+      prevObserverOptions.current = observerOptions;
+    }
+  }, [observerTarget, observerRoot]);
+
+  return { intersectionEntry };
+}
