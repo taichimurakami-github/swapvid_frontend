aa295aa02767ec5ce405e1eac314a01f6071a475
Taichi.Murakami <tchi0017@gmail.com>
2023-10-05 16:10:27 +0900

    Make component container and eliminate the close button

--- src/containers/DraggableVideoContainer.tsx
+++ src/containers/DraggableVideoContainer.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useRef, useState } from "react";
+import React, { PropsWithChildren, useRef } from "react";
 import Draggable from "react-draggable";
 import { useHandleClickWithDrag } from "@/hooks/useDraggable";
 import { useVideoCurrenttime } from "@/hooks/useVideoCurrenttime";
@@ -12,7 +12,7 @@ import useIntersectionObserver from "@/hooks/useIntersectionObserver";
  * please update virtual DOM by inserting and deleting DraggableVideo
  * (It wouldn't cause performance issues cause DraggableVideo uses component memorization)
  */
-const DraggableVideo = React.memo(function _DraggableVideo(
+const DraggableVideoContainer = React.memo(function _DraggableVideo(
   props: PropsWithChildren<{
     active: boolean;
     videoElement: HTMLVideoElement | null;
@@ -21,18 +21,10 @@ const DraggableVideo = React.memo(function _DraggableVideo(
     onHandleClose?: () => void;
   }>
 ) {
-  const CLOSE_BTN_OPACITY = {
-    unactive: 0,
-    visible: 50,
-    active: 100,
-  };
-
   const smallVideoRef = useRef<HTMLVideoElement>(null);
   const videoCurrentTime = useVideoCurrenttime(props.videoElement);
   const eventHandlers = useHandleClickWithDrag(props.onHandleClick, 200);
   const wrapperElemRef = useRef(null);
-
-  const [closeBtnOpacity, setCloseBtnOpacity] = useState(0);
 
   const syncParentVideoCurrentTime = (target: HTMLVideoElement) => {
     target.currentTime = videoCurrentTime;
@@ -57,12 +49,6 @@ const DraggableVideo = React.memo(function _DraggableVideo(
           props.active ? "active" : "unactive"
         }`}
         ref={wrapperElemRef}
-        onPointerEnter={() => {
-          setCloseBtnOpacity(CLOSE_BTN_OPACITY.visible);
-        }}
-        onPointerLeave={() => {
-          setCloseBtnOpacity(CLOSE_BTN_OPACITY.unactive);
-        }}
       >
         <video
           className={`relative w-full border-4`}
@@ -79,29 +65,9 @@ const DraggableVideo = React.memo(function _DraggableVideo(
           className="_dragger-onclick-handler absolute top-0 left-0 w-full h-full"
           {...eventHandlers}
         ></div>
-        {closeBtnOpacity > 0 && (
-          <div
-            className="absolute top-[3%] right-[3%] flex-xyc rounded-full bg-slate-600 w-[3rem] h-[3rem] text-white text-3xl font-bold"
-            style={{
-              opacity: `${closeBtnOpacity}%`,
-            }}
-            onPointerEnter={() => {
-              setCloseBtnOpacity(CLOSE_BTN_OPACITY.active);
-            }}
-            onPointerLeave={() => {
-              setCloseBtnOpacity(CLOSE_BTN_OPACITY.visible);
-            }}
-            onClick={props.onHandleClose}
-          >
-            <div className="relative w-full h-full">
-              <span className="absolute rotate-45 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 block bg-white w-[60%] h-[5px]"></span>
-              <span className="absolute -rotate-45 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 block bg-white w-[60%] h-[5px]"></span>
-            </div>
-          </div>
-        )}
       </div>
     </Draggable>
   );
 });
 
-export default DraggableVideo;
+export default DraggableVideoContainer;
