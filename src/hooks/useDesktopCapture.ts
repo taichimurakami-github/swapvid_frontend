804748f8e1dc3e620a8ab22e0baab84701237674
taichi.murakami <tchi0017@gmail.com>
2023-10-29 04:57:44 +0900

    Add desktop capture tool

---
+++ src/hooks/useDesktopCapture.ts
@@ -0,0 +1,31 @@
+import React, { useCallback } from "react";
+
+export function useDesktopCapture(videoRef: React.RefObject<HTMLVideoElement>) {
+  const startCaptureDesktop = useCallback(async () => {
+    try {
+      const captureStream = await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: true,
+      });
+
+      const video = videoRef.current;
+
+      if (!video) throw new Error("Cannot find video element.");
+
+      video.width =
+        captureStream.getVideoTracks()[0].getSettings().width ?? 1000;
+      video.height =
+        captureStream.getVideoTracks()[0].getSettings().height ?? 1000;
+      video.srcObject = captureStream;
+      video.onloadeddata = () => {
+        video.play();
+      };
+
+      // console.log(captureStream);
+    } catch (e) {
+      console.log(e);
+    }
+  }, [videoRef]);
+
+  return { startCaptureDesktop };
+}
