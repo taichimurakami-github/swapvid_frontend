ca1de11b884202e7626e70862c4a0e28768efc6c
taichi.murakami <tchi0017@gmail.com>
2023-11-06 23:35:25 +0900

    Add an endpoint for SQA on docker container

--- src/app.config.ts
+++ src/app.config.ts
@@ -4,7 +4,8 @@
 
 export const ACTIVE_ASSET_ID_LS_CACHE_KEY = "__lastActiveAssetId";
 
-export const SEQUENCE_ANALYZER_API_ENDPOINT = "http://localhost:8881/";
+// export const SEQUENCE_ANALYZER_API_ENDPOINT = "http://localhost:8881/";
+export const SEQUENCE_ANALYZER_API_ENDPOINT = "http://0.0.0.0:8881/";
 
 export const ASSET_ID_LIST = [
   /**
