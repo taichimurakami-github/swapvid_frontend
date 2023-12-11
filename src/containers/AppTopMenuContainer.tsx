6f51cc27c4697619c5ce4c1d7dca59230c232ae2
taichi.murakami <tchi0017@gmail.com>
2023-12-11 18:40:57 +0900

    Add toggle to show/hide AppTopMenu

--- src/containers/AppTopMenuContainer.tsx
+++ src/containers/AppTopMenuContainer.tsx
@@ -7,9 +7,21 @@ export const AppTopMenuContainer = (
     activeAssetId: TAssetId;
     handleChangeActiveAssetId: (assetId: TAssetId) => void;
     handleChangeInterfaceMode: (mode: TInterfaceMode) => void;
+
+    hideCombinedLiveViewButton?: boolean;
+    hideCombinedLocalViewButton?: boolean;
+    hideParallelLocalViewButton?: boolean;
   }>
 ) => {
+  const [appTopMenuVisible, setAppTopMenuVisible] = useState(false);
   const [assetChangeFormVisible, setAssetChangeFormVisible] = useState(false);
+
+  const showAppTopMenu = useCallback(() => {
+    setAppTopMenuVisible(true);
+  }, [setAppTopMenuVisible]);
+  const hideAppTopMenu = useCallback(() => {
+    setAppTopMenuVisible(false);
+  }, [setAppTopMenuVisible]);
 
   const handleChangeAssetSelectFormActive = useCallback(
     (value?: boolean) => {
@@ -35,69 +47,67 @@ export const AppTopMenuContainer = (
 
   return (
     <>
-      <div className="absolute top-0 left-0 flex-xyc gap-4 text-center text-white text-md p-2">
-        <h1 className="flex-xyc gap-4 bg-neutral-800 text-center text-white">
-          <p className="flex-xyc gap-2">
-            Playing: <b>{props.activeAssetId}</b>
-          </p>
-          <button
-            className="p-2 rounded-md font-bold bg-slate-600 hover:bg-slate-500"
-            onClick={() => {
-              handleChangeAssetSelectFormActive();
-            }}
-          >
-            Change Asset
-          </button>
-          <button
-            className="p-2 rounded-md font-bold bg-slate-600 hover:bg-slate-500"
-            onClick={handleClickInterfaceModeToCombinedLSBtn}
-          >
-            Combined (Live)
-          </button>
-          <button
-            className="p-2 rounded-md font-bold bg-slate-600 hover:bg-slate-500"
-            onClick={handleClickInterfaceModeToCombinedBtn}
-          >
-            Combined (Local)
-          </button>
-          <button
-            className={"p-2 rounded-md font-bold bg-slate-600 cursor-auto"}
-            onClick={handleClickInterfaceModeToParallelBtn}
-          >
-            Parallel (Local)
-          </button>
-          {/* <button
+      {appTopMenuVisible ? (
+        <div className="absolute top-0 left-0 flex-xyc gap-4 text-center text-white text-md p-2">
+          <h1 className="flex-xyc gap-4 bg-neutral-800 text-center text-white">
+            <p className="flex-xyc gap-2">
+              Playing: <b>{props.activeAssetId}</b>
+            </p>
+            <button
+              className="p-2 rounded-md font-bold bg-slate-600 hover:bg-slate-500"
+              onClick={() => {
+                handleChangeAssetSelectFormActive();
+              }}
+            >
+              Change Asset
+            </button>
+            {!props.hideCombinedLiveViewButton && (
+              <button
+                className="p-2 rounded-md font-bold bg-slate-600 hover:bg-slate-500"
+                onClick={handleClickInterfaceModeToCombinedLSBtn}
+              >
+                SwapVid (Live)
+              </button>
+            )}
+            {!props.hideCombinedLocalViewButton && (
+              <button
+                className="p-2 rounded-md font-bold bg-slate-600 hover:bg-slate-500"
+                onClick={handleClickInterfaceModeToCombinedBtn}
+              >
+                SwapVid (Local)
+              </button>
+            )}
+            {!props.hideParallelLocalViewButton && (
+              <button
+                className={"p-2 rounded-md font-bold bg-slate-600"}
+                onClick={handleClickInterfaceModeToParallelBtn}
+              >
+                Side-by-side (Local)
+              </button>
+            )}
+            <button
+              className={"p-2 rounded-md font-bold bg-red-700"}
+              onClick={hideAppTopMenu}
+            >
+              Close App Menu
+            </button>
+            {/* <button
                     className="p-2 rounded-md font-bold bg-slate-600 hover:bg-slate-500"
                     onClick={handleFullScreen}
                   >
                     {isFullScreen ? "Exit full screen" : "Request full screen"}
-                  </button> */}
-        </h1>
-        {/* <button
-                className={
-                  interfaceModeState === "parallel"
-                    ? "p-2 rounded-md font-bold bg-red-600 cursor-auto"
-                    : "p-2 rounded-md font-bold hover:bg-slate-500"
-                }
-                onClick={() => {
-                  handleOnChangeInterfaceMode("parallel");
-                }}
-              >
-                Parallel View
-              </button>
-              <button
-                className={
-                  interfaceModeState === "combined"
-                    ? "p-2 rounded-md font-bold bg-red-600 cursor-auto"
-                    : "p-2 rounded-md font-bold hover:bg-slate-500"
-                }
-                onClick={() => {
-                  handleOnChangeInterfaceMode("combined");
-                }}
-              >
-                Combined View
-              </button> */}
-      </div>
+                </button> */}
+          </h1>
+        </div>
+      ) : (
+        <button
+          className="fixed top-0 left-0 flex-xyc w-full h-[50px] p-2 font-bold text-white text-xl bg-black-transparent-01 opacity-0 hover:opacity-100"
+          onClick={showAppTopMenu}
+        >
+          Show App Menu
+        </button>
+      )}
+
       {assetChangeFormVisible && (
         <div
           className="fixed z-90 top-0 left-0 w-full h-full bg-black-transparent-01 flex-xyc z-10"
