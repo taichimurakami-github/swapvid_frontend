e26d308b4cf08c635dbd2a2cde75074f4f989ef2
taichi.murakami <tchi0017@gmail.com>
2024-01-09 03:27:58 +0900

    Add vite-pwa plugin and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   base: "/",
   server: {
     host: "0.0.0.0",
