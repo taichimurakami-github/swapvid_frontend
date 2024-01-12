1da84df6969eccf172bed03d82196e6ad82116dc
taichi.murakami <tchi0017@gmail.com>
2024-01-12 14:05:54 +0900

    Remove runtime asset importer for build

--- src/containers/SwapVidPlayerRoot.tsx
+++ src/containers/SwapVidPlayerRoot.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useCallback } from "react";
 import { useAtomValue, useSetAtom } from "jotai/react";
-import { useAssetData } from "@/hooks/useAssetDataLoader";
 import {
   PlayerCombinedView,
   PlayerParallelView,
@@ -8,16 +7,11 @@ import {
 import {
   assetIdAtom,
   assetLoaderStateAtom,
-  documentOverviewImgSrcAtom,
   localFilePickerActiveAtom,
-  pdfSrcAtom,
-  preGeneratedScrollTimelineDataAtom,
-  subtitlesDataAtom,
   swapvidDesktopEnabledAtom,
   swapvidInterfaceTypeAtom,
-  videoSrcAtom,
-} from "@/providers/jotai/swapVidPlayer";
-import { TAssetId } from "@/types/swapvid";
+} from "@/providers/jotai/store";
+// import { TAssetId } from "@/types/swapvid";
 
 export const SwapVidPlayerRoot: React.FC<{
   mediaSourceType?: "streaming" | "local" | "presets";
@@ -29,86 +23,17 @@ export const SwapVidPlayerRoot: React.FC<{
   const swapvidDesktopEnabled = useAtomValue(swapvidDesktopEnabledAtom);
   const assetLoaderState = useAtomValue(assetLoaderStateAtom);
 
-  const setPdfSrc = useSetAtom(pdfSrcAtom);
-  const setVideoSrc = useSetAtom(videoSrcAtom);
-  const setDocumentOverviewImgSrc = useSetAtom(documentOverviewImgSrcAtom);
-  const setSubtitlesData = useSetAtom(subtitlesDataAtom);
-  const setPreGeneratedScrollTimelineData = useSetAtom(
-    preGeneratedScrollTimelineDataAtom
-  );
   const setLocalFilePickerActive = useSetAtom(localFilePickerActiveAtom);
 
-  /** Load Assets Here */
-  const {
-    loadDocumentOverviewImgSrc,
-    loadPdfSrc,
-    loadVideoSrc,
-    loadSubtitlesData,
-    loadPreGeneratedScrollTimelineData,
-  } = useAssetData();
-
-  const importDefaultAssetsOnRuntime = useCallback(
-    async (
-      assetId: TAssetId,
-      options?: {
-        skipLoadPdfSrc?: boolean;
-        skipLoadVideoSrc?: boolean;
-      }
-    ) => {
-      const [
-        documentOverviewImgSrc,
-        subtitlesData,
-        preGeneratedScrollTimelineData,
-      ] = await Promise.all([
-        loadDocumentOverviewImgSrc(assetId),
-        loadSubtitlesData(assetId),
-        loadPreGeneratedScrollTimelineData(assetId),
-      ]);
-
-      const videoSrc = options?.skipLoadVideoSrc
-        ? null
-        : await loadVideoSrc(assetId);
-      const pdfSrc = options?.skipLoadPdfSrc ? null : await loadPdfSrc(assetId);
-
-      setPdfSrc(pdfSrc);
-      setVideoSrc(videoSrc);
-      setDocumentOverviewImgSrc(documentOverviewImgSrc);
-      setSubtitlesData(subtitlesData);
-      setPreGeneratedScrollTimelineData(preGeneratedScrollTimelineData);
-    },
-    [
-      loadDocumentOverviewImgSrc,
-      loadPdfSrc,
-      loadVideoSrc,
-      loadSubtitlesData,
-      loadPreGeneratedScrollTimelineData,
-      setPdfSrc,
-      setVideoSrc,
-      setDocumentOverviewImgSrc,
-      setSubtitlesData,
-      setPreGeneratedScrollTimelineData,
-    ]
-  );
-
   useEffect(() => {
-    assetId &&
-      importDefaultAssetsOnRuntime(assetId, {
-        skipLoadVideoSrc: !assetLoaderState.video.presetsEnabled,
-        skipLoadPdfSrc: !assetLoaderState.video.presetsEnabled,
-      });
+    const localSourceExists = Object.values(assetLoaderState)
+      .map((v) => v.sourceType)
+      .includes("local");
 
-    if (
-      !assetLoaderState.video.presetsEnabled ||
-      !assetLoaderState.video.presetsEnabled
-    ) {
+    if (localSourceExists) {
       setLocalFilePickerActive(true);
     }
-  }, [
-    assetId,
-    importDefaultAssetsOnRuntime,
-    assetLoaderState,
-    swapvidDesktopEnabled,
-  ]);
+  }, [assetId, assetLoaderState, swapvidDesktopEnabled]);
 
   switch (interfaceType) {
     case "combined":
--- src/hooks/useAssetDataLoader.ts
+++ src/hooks/useAssetDataLoader.ts
@@ -4,7 +4,34 @@ import {
   TSubtitlesData,
 } from "@/types/swapvid";
 import { useCallback } from "react";
-import { loadAssetData } from "@utils/assetDataUtil";
+
+/**
+ * Assets loader using dynamic import.
+ * Can be used for runtime loading of assets.
+ * This feature is currently deprecated due to significantly increasing the bundle size.
+ */
+
+/**
+ * Helper for importing assets
+ * using v3 dynamic import
+ */
+const loadAssetData = <T>(
+  assetId: TAssetId,
+  filenameWoExt: string,
+  ext: string
+): Promise<T | null> => {
+  // [WARNING]
+  // Dynamic import doesn't work except "/" just before filename,
+  // and "." between filename and extension.
+  // Also, "import" syntax needs literal string, not valiable of string.
+  //
+  return import(`@assets/${assetId}/${filenameWoExt}.${ext}`)
+    .then((v) => v.default as T)
+    .catch((e) => {
+      console.log(e);
+      return null;
+    });
+};
 
 export function useAssetData() {
   const loadVideoSrc = useCallback(
