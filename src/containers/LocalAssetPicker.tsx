688c8beb875f535bc42b0f9f4da67fbf38e6f227
taichi.murakami <tchi0017@gmail.com>
2024-01-12 16:48:59 +0900

    Update asset picker

--- src/containers/LocalAssetPicker.tsx
+++ src/containers/LocalAssetPicker.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useReducer } from "react";
 import {
+  documentOverviewImgSrcAtom,
   localFilePickerActiveAtom,
   pdfSrcAtom,
   preGeneratedScrollTimelineDataAtom,
@@ -8,6 +9,18 @@ import {
 import { useAtom, useSetAtom } from "jotai/react";
 import { AppModalTypeA, AppModalWrapper } from "@/presentations/Modal";
 import { useMultipleFilesInput } from "@/hooks/useFileInput";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+
+type PickedAssetState<T> = {
+  video: T;
+  document: T;
+  overviewImage: T;
+  scrollTimeline: T;
+};
+
+type SelectedAssetState = PickedAssetState<boolean>;
+type ErrorMessageState = PickedAssetState<string>;
 
 /**
  * Load large files
@@ -16,11 +29,38 @@ export const LocalAssetPicker: React.FC<{ zIndex?: number }> = ({ zIndex }) => {
   // const { video: loaderStateVideo, pdf: loaderStatePdf } =
   //   useAtomValue(assetLoaderStateAtom);
 
+  const [selectedAsset, dispatchSelectedAsset] = useReducer(
+    (b: SelectedAssetState, payload: Partial<SelectedAssetState>) => ({
+      ...b,
+      ...payload,
+    }),
+    {
+      video: false,
+      document: false,
+      overviewImage: false,
+      scrollTimeline: false,
+    }
+  );
+
+  const [errorMessage, dispatchErrorMessage] = useReducer(
+    (b: ErrorMessageState, payload: Partial<ErrorMessageState>) => ({
+      ...b,
+      ...payload,
+    }),
+    {
+      video: "",
+      document: "",
+      overviewImage: "",
+      scrollTimeline: "",
+    }
+  );
+
   const setVideoSrc = useSetAtom(videoSrcAtom);
   const setPdfSrc = useSetAtom(pdfSrcAtom);
   const setPreGeneratedScrollTimelineData = useSetAtom(
     preGeneratedScrollTimelineDataAtom
   );
+  const setDocumentOverviewImgSrc = useSetAtom(documentOverviewImgSrcAtom);
 
   const [active, setActive] = useAtom(localFilePickerActiveAtom);
 
@@ -29,32 +69,74 @@ export const LocalAssetPicker: React.FC<{ zIndex?: number }> = ({ zIndex }) => {
   // const scrollTimelineInput = useFileInput();
 
   const { files, handleOnInputChange } = useMultipleFilesInput();
-  const [errorMessage, setErrorMessage] = useState("");
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      handleOnInputChange(e);
+
+      if (!e.target.files) {
+        return;
+      }
+
+      /** Requirements validation */
+      const validationResult: SelectedAssetState = {
+        video: false,
+        document: false,
+        overviewImage: false,
+        scrollTimeline: true,
+      };
+
+      for (const f of e.target.files as FileList) {
+        validationResult.video =
+          validationResult.video || f.name.includes(".mp4");
+        validationResult.document =
+          validationResult.document || f.name.includes(".pdf");
+        validationResult.overviewImage =
+          validationResult.overviewImage ||
+          f.name.includes(".png") ||
+          f.name.includes(".jpg");
+      }
+
+      Object.keys(errorMessage).map((key) =>
+        dispatchErrorMessage({
+          [key]: validationResult[key as keyof SelectedAssetState]
+            ? ""
+            : `${key} file is required.`,
+        })
+      );
+
+      dispatchSelectedAsset({
+        video: false,
+        document: false,
+        overviewImage: false,
+        scrollTimeline: false,
+      });
+      /** Update selectedAssetState */
+      for (const f of e.currentTarget.files as FileList) {
+        const ext = f.name.split(".").pop();
+        switch (ext) {
+          case "mp4":
+            dispatchSelectedAsset({ video: true });
+            break;
+
+          case "pdf":
+            dispatchSelectedAsset({ document: true });
+            break;
+
+          case "json":
+            dispatchSelectedAsset({ scrollTimeline: true });
+            break;
+
+          case "png":
+          case "jpg":
+            dispatchSelectedAsset({ overviewImage: true });
+            break;
+        }
+      }
+    },
+    [handleOnInputChange]
+  );
 
   const handleSubmit = useCallback(async () => {
-    /** Validation */
-    const validationResult = {
-      mp4Included: false,
-      pdfIncluded: false,
-    };
-    for (const f of files as FileList) {
-      validationResult.mp4Included =
-        validationResult.mp4Included || f.name.includes(".mp4");
-      validationResult.pdfIncluded =
-        validationResult.pdfIncluded || f.name.includes(".pdf");
-    }
-
-    if (Object.values(validationResult).filter((v) => !v).length > 0) {
-      const message = [
-        !validationResult.mp4Included && ".mp4 file",
-        !validationResult.pdfIncluded && ".pdf file",
-      ]
-        .filter((v) => v)
-        .join(" and ");
-
-      return setErrorMessage(`Please include ${message}`);
-    }
-
     /** Registration */
     for (const f of files as FileList) {
       const ext = f.name.split(".").pop();
@@ -63,23 +145,42 @@ export const LocalAssetPicker: React.FC<{ zIndex?: number }> = ({ zIndex }) => {
         case "mp4":
           setVideoSrc(URL.createObjectURL(f));
           break;
+
         case "pdf":
           setPdfSrc(URL.createObjectURL(f));
           break;
+
         case "json":
           f.name.includes(".scroll.json") &&
             setPreGeneratedScrollTimelineData(JSON.parse(await f.text()));
+          break;
+
+        case "png":
+        case "jpg":
+        case "JPEG":
+        case "JPG":
+        case "webp":
+        case "svg":
+          setDocumentOverviewImgSrc(URL.createObjectURL(f));
+          break;
       }
     }
 
     setActive(false);
   }, [
+    selectedAsset,
     setActive,
+    files,
     setVideoSrc,
     setPdfSrc,
     setPreGeneratedScrollTimelineData,
-    files,
+    setDocumentOverviewImgSrc,
   ]);
+
+  const submissionReady =
+    selectedAsset.video &&
+    selectedAsset.document &&
+    selectedAsset.overviewImage;
 
   const EscapedAssetIdSymbol = (
     <span className="inline-block mx-2 text-teal-600">
@@ -89,39 +190,49 @@ export const LocalAssetPicker: React.FC<{ zIndex?: number }> = ({ zIndex }) => {
 
   return (
     <AppModalWrapper visibility={active} zIndex={zIndex}>
-      <AppModalTypeA title="Please choose assets">
+      <AppModalTypeA title="Asset Picker">
         <div className="grid justify-center gap-8 text-xl">
-          <h2 className="text-center font-bold">Select the Asset Files</h2>
+          <h2 className="text-center font-bold">
+            Pleaase select required asset files.
+          </h2>
 
-          <p className="flex gap-2">
-            Requirements:
-            <b>
-              ①{EscapedAssetIdSymbol}.mp4, ②{EscapedAssetIdSymbol}
-              .pdf
-            </b>
-          </p>
-          <p>
-            Optional: <b>{EscapedAssetIdSymbol}.scroll.json</b>
-          </p>
+          <div className="flex flex-col gap-6 items-start">
+            <Item
+              title="[REQUIRED] Video File (.mp4)"
+              errorMessage={errorMessage.video}
+              value={selectedAsset.video}
+            />
+            <Item
+              title="[REQUIRED] Document File (.pdf)"
+              errorMessage={errorMessage.document}
+              value={selectedAsset.document}
+            />
+            <Item
+              title="[REQUIRED] Document Overview Image (Any image format)"
+              errorMessage={errorMessage.overviewImage}
+              value={selectedAsset.overviewImage}
+            />
+            <Item
+              title="[OPTIONAL] Scroll Timeline (.json)"
+              errorMessage={errorMessage.scrollTimeline}
+              value={selectedAsset.scrollTimeline}
+            />
+          </div>
 
           <input
             className="bg-slate-200 p-2 rounded-sm hover:bg-slate-300"
             type="file"
-            accept="video/*,.pdf,.json"
-            onChange={handleOnInputChange}
+            accept="video/*,image/*,.pdf,.json"
+            onChange={handleChange}
             maxLength={3}
             minLength={2}
             multiple
           />
 
-          {errorMessage && (
-            <p className="text-red-600 text-center font-bold">{errorMessage}</p>
-          )}
-
           <button
             className="w-28 p-2 mx-auto bg-teal-600 hover:bg-teal-700 disabled:opacity-40 text-xl text-white font-bold rounded-full"
             // disabled={!videoInput.file}
-            disabled={!files}
+            disabled={!submissionReady}
             onClick={handleSubmit}
           >
             OK
@@ -131,3 +242,24 @@ export const LocalAssetPicker: React.FC<{ zIndex?: number }> = ({ zIndex }) => {
     </AppModalWrapper>
   );
 };
+
+const Item: React.FC<{
+  title: string;
+  errorMessage?: string;
+  value: boolean;
+}> = ({ title, errorMessage, value }) => (
+  <div className="flex-xyc gap-4">
+    <div
+      className="bg-teal-600 text-white rounded-full w-8 h-8 flex-xyc font-bold"
+      style={{
+        opacity: value ? 1 : 0.3,
+      }}
+    >
+      <FontAwesomeIcon icon={faCheck} />
+    </div>
+    <p className="grid">
+      <span className="text-lg">{title}</span>
+      <b className="text-red-600 text-md">{errorMessage ?? ""}</b>
+    </p>
+  </div>
+);
